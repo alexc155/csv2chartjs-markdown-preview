18d59820693621be59ca23362bc502d5aa0eb5f2
fgiger <florian.giger@gris.informatik.tu-darmstadt.de>
2019-02-18 21:26:29 +0100

    this and that

--- graphviz-render.js
+++ graphviz-render.js
@@ -1,28 +1,20 @@
 function contentLoaded() {
-    var graphvizElements = document.getElementsByClassName('graphviz');
+  var chartElements = document.getElementsByClassName("chartjs");
 
-    var changes = [];
+  for (let index = 0; index < chartElements.length; index++) {
+    var element = chartElements.item(index);
+    var source = element.textContent;
 
-    for (let index = 0; index < graphvizElements.length; index++) {
-        var element = graphvizElements.item(index);
-        var source = element.textContent;
-
-        changes.push({
-            placeholder: element.parentElement.parentElement,
-            svg: Viz(source)
-        });
-    }
-
-    for (let index = 0; index < changes.length; index++) {
-        const element = changes[index];
-        element.placeholder.outerHTML = element.svg;
-    }
-
-    // use to debug rendered code.
-    // document.body.appendChild(document.createTextNode(document.body.innerHTML));
+    new Chart(element.getContext("2d"), source);
+  }
+  // use to debug rendered code.
+  document.body.appendChild(document.createTextNode(document.body.innerHTML));
 }
 
-window.addEventListener('load', function () {
-    contentLoaded()
-}, false)
-
+window.addEventListener(
+  "load",
+  function() {
+    contentLoaded();
+  },
+  false
+);
--- src/extension.ts
+++ src/extension.ts
@@ -1,39 +1,34 @@
-'use strict';
+"use strict";
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "Chartjs-markdown-preview" is now active!'
+  );
+  return {
+    extendMarkdownIt(md) {
+      const highlight = md.options.highlight;
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "graphviz-markdown-preview" is now active!');
-    return {
-
-        extendMarkdownIt(md) {
-
-            const highlight = md.options.highlight;
-
-            md.options.highlight = (code, lang) => {
-
-                if (lang && lang.match(/\bgraphviz\b/i)) {
-
-                    return `<div class="graphviz">${code}</div>`;
-                }
-
-                return highlight(code, lang);
-
-            };
-
-            return md;
-
+      md.options.highlight = (code, lang) => {
+        console.log("Lang is", lang);
+        console.log("Code is", code.trim());
+        if (lang && lang.match(/\bchart\b/i)) {
+          return `<canvas class="chartjs">${code}</canvas>`;
         }
 
+        return highlight(code, lang);
+      };
+
+      return md;
     }
+  };
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
